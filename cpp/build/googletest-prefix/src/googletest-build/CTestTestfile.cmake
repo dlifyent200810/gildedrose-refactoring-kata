00f5d2b375675955bfeeed4d98fc4128de8b236b
Krzysztof Konarski <konarskikrzysztof1@gmail.com>
2021-12-14 14:23:07 +0100

    Simplified first nested if

---
+++ cpp/build/googletest-prefix/src/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/konis/Desktop/Bakcyl/Zadania/Zadanie 2/gildedrose-refactoring-kata/cpp/build/googletest-prefix/src/googletest
+# Build directory: /home/konis/Desktop/Bakcyl/Zadania/Zadanie 2/gildedrose-refactoring-kata/cpp/build/googletest-prefix/src/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
