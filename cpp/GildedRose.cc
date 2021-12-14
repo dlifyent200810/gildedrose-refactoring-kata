e6bfdae52aea041fd41f5b08c6aa8d5c023bb167
Jacek Jarosz <jacekjarosz55@gmail.com>
2021-12-14 15:45:14 +0100

    HUGE refactor made by going through every path and writing down instructions by putting the item.name as the first important factor

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -1,77 +1,68 @@
 #include "GildedRose.h"
 #include <string>
 
-
 GildedRose::GildedRose(::std::vector<Item> const& items) : items(items)
 {}
 
 GildedRose::GildedRose(::std::vector<Item> && items) : items(::std::move(items))
 {}
 
-
 const std::string AGED_BRIE = "Aged Brie";
 const std::string BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
 const std::string SULFURAS =  "Sulfuras, Hand of Ragnaros";
-
 
 void GildedRose::updateQuality()
 {
     for (Item& item : items)
     {
-        if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASS)
-        {
-            if (item.quality > 0 && item.name != SULFURAS)
-            {
-                    item.quality--;
-            }
-        }
-        else
+        if(item.name == AGED_BRIE)
         {
             if (item.quality < 50)
             {
                 item.quality++;
+            }
 
-                if (item.name == BACKSTAGE_PASS)
-                {
-                    if (item.sellIn < 11 && item.quality < 50)
+            item.sellIn--;
+            if (item.sellIn < 0)
+            {
+                    if (item.quality < 50)
                     {
-                        if(item.sellIn < 6)
-                        {
-                            item.quality++;
-                        }
                         item.quality++;
                     }
-                }
             }
         }
 
-        if (item.name != SULFURAS)
+        else if (item.name == BACKSTAGE_PASS) 
         {
-            item.sellIn--;
-        }
-
-        if (item.sellIn < 0)
-        {
-            if (item.name !=AGED_BRIE )
+            if(item.quality < 50)
             {
-                if (item.name != BACKSTAGE_PASS)
+                item.quality++;
+
+                if (item.sellIn < 11)
                 {
-                    if (item.quality > 0 && item.name != SULFURAS)
+                    if(item.sellIn < 6)
                     {
-                            item.quality--;
+                        item.quality++;
                     }
-                }
-                else
-                {
-                    item.quality = 0;
-                }
-            }
-            else
-            {
-                if (item.quality < 50)
-                {
                     item.quality++;
                 }
+            }
+            item.sellIn--;
+            if (item.sellIn < 0)
+            {
+                item.quality = 0; 
+            }
+        }
+
+        else if (item.name != SULFURAS) {
+            item.sellIn--;
+            if (item.quality > 0)
+            {
+                    item.quality--;
+                    if (item.sellIn < 0)
+                    {
+                                item.quality--;
+                    }
             }
         }
     }
