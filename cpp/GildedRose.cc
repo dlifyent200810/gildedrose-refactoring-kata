aef73df5b54f8431b8eecf4dd4f3eee5b0a160ee
Jacek Jarosz <jacekjarosz55@gmail.com>
2021-12-14 14:11:36 +0100

    Put item names into const string variables.

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -1,4 +1,5 @@
 #include "GildedRose.h"
+#include <string>
 
 
 GildedRose::GildedRose(::std::vector<Item> const& items) : items(items)
@@ -7,15 +8,21 @@ GildedRose::GildedRose(::std::vector<Item> const& items) : items(items)
 GildedRose::GildedRose(::std::vector<Item> && items) : items(::std::move(items))
 {}
 
+
+const std::string AGED_BRIE = "Aged Brie";
+const std::string BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+const std::string SULFURAS =  "Sulfuras, Hand of Ragnaros";
+
+
 void GildedRose::updateQuality()
 {
     for (int i = 0; i < items.size(); i++)
     {
-        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+        if (items[i].name != AGED_BRIE && items[i].name != BACKSTAGE_PASS)
         {
             if (items[i].quality > 0)
             {
-                if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                if (items[i].name != SULFURAS)
                 {
                     items[i].quality = items[i].quality - 1;
                 }
@@ -27,7 +34,7 @@ void GildedRose::updateQuality()
             {
                 items[i].quality = items[i].quality + 1;
 
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
+                if (items[i].name == BACKSTAGE_PASS)
                 {
                     if (items[i].sellIn < 11)
                     {
@@ -48,20 +55,20 @@ void GildedRose::updateQuality()
             }
         }
 
-        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+        if (items[i].name != SULFURAS)
         {
             items[i].sellIn = items[i].sellIn - 1;
         }
 
         if (items[i].sellIn < 0)
         {
-            if (items[i].name != "Aged Brie")
+            if (items[i].name !=AGED_BRIE )
             {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+                if (items[i].name != BACKSTAGE_PASS)
                 {
                     if (items[i].quality > 0)
                     {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                        if (items[i].name != SULFURAS)
                         {
                             items[i].quality = items[i].quality - 1;
                         }
