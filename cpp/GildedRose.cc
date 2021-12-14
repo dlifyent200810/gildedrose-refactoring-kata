a4c781cc140050bd8b1d6a9915f41b9bee3273ac
asterikg <tusiek15243@gmail.com>
2021-12-14 15:15:25 +0100

    Replacing magic string with named constants

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -7,11 +7,16 @@ GildedRose::GildedRose(::std::vector<Item> const& items) : items(items)
 GildedRose::GildedRose(::std::vector<Item> && items) : items(::std::move(items))
 {}
 
+static const char *const BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+static const char *const REGULAR = "Foo";
+static const char *const BRIE = "Aged Brie";
+static const char *const SULFURAS = "Sulfuras, Hand of Ragnaros";
+
 void GildedRose::updateQuality()
 {
     for (int i = 0; i < items.size(); i++)
     {
-        if (items[i].name == "Foo")
+        if (items[i].name == REGULAR)
         {
             if (items[i].quality > 0)
             {
@@ -26,7 +31,7 @@ void GildedRose::updateQuality()
             {
                 items[i].quality++;
 
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
+                if (items[i].name == BACKSTAGE_PASS)
                 {
                     if (items[i].sellIn < 11)
                     {
@@ -47,20 +52,20 @@ void GildedRose::updateQuality()
             }
         }
 
-        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+        if (items[i].name != SULFURAS)
         {
             items[i].sellIn--;
         }
 
         if (items[i].sellIn < 0)
         {
-            if (items[i].name != "Aged Brie")
+            if (items[i].name != BRIE)
             {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+                if (items[i].name != BACKSTAGE_PASS)
                 {
                     if (items[i].quality > 0)
                     {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                        if (items[i].name != SULFURAS)
                         {
                             items[i].quality--;
                         }
