93930bbf53f34759a6b262ada1c1221eae0c9e95
dlifyent200810 <150160948+dlifyent200810@users.noreply.github.com>
2023-11-20 18:14:51 +0100

    Update GildedRose.h

--- cpp/GildedRose.h
+++ cpp/GildedRose.h
@@ -13,12 +13,17 @@ public:
 
 class GildedRose
 {
+private:
+    const int fifty = 50;
+    const string AB = "Aged Brie";
+    const string BPTATC = "Backstage passes to a TAFKAL80ETC concert";
+    const string SHOR = "Sulfuras, Hand of Ragnaros";
+    void increaseItemAtIndexByOne(int sellInOfItem); 
 
 public:
     ::std::vector<Item> items;
     explicit GildedRose(::std::vector<Item> && items);
     explicit GildedRose(::std::vector<Item> const& items);
-
+    
     void updateQuality();
 };
-
